6afd1b93b9a3674d7bee787fbaf17ea945b9c970
Giayychan <giayanny926@gmail.com>
2020-03-02 09:46:53 +0800

    changed

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,8 +3,8 @@ require('../models/users')
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-	console.log('req.query.houseID', req.query.houseID)
-	Reviews.find({ house: req.query.houseID })
+	console.log('req.query', req.query)
+	Reviews.find(req.query)
 		.populate('author')
 		.then(reviews => {
 			res.send(reviews)
